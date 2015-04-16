448a8b26f4bdda1d5e20120b56c21d7e4a06dd94
Justin Stewart <stewartjustin3@gmail.com>
2015-04-15 20:16:45 -0400

    finished working application

--- js/app.js
+++ js/app.js
@@ -1,43 +1,87 @@
 //declare variables //
-var quiz1, quiz2, quizArray, questionField;
+var quiz1, quiz2, quizArray, questionArray, questionField, questionNumber, submitButton, response, points;
 
 
 // quiz objects //
-var quiz1 = {
+quizQuestion0 = {
   question : "What is the Capital of Kansas? ",
   options : ["Topeka", "Des Moines", "Iowa City"],
   answer  : "Topeka"
 }
 
-var quiz2 = {
+quizQuestion1 = {
   question : "What is the Capital of Kentucky? ",
-  option1 : "Lexington",
-  option2 : "Des Moines",
-  option3 : "Iowa City"
+  options : ["Lexington", "Nashville",  "Bowling Greens"],
+  answer : "Lexington"
 }
 
-var quizArray = [quiz1, quiz2]
-
-
-document.getElementById('question').innerHTML = quizArray[0].question;
-
-questionField = document.getElementById('question-options');
-
-
-for(x = 0; x < quizArray[0].options.length; x++){
-  var input  = document.createElement('input');
-  var label  = document.createElement('label');
-  var lineBreak = document.createElement('br');
-  input.type = "radio";
-  input.name = "question1";
-  input.value = quizArray[0].options[x];
-  input.id = quizArray[0].options[x];
-  questionField.appendChild(input);
-  label.innerHTML = quizArray[0].options[x];
-  label.htmlFor = quizArray[0].options[x];
-  questionField.appendChild(label);
-  questionField.appendChild(lineBreak);
+quizQuestion2 = {
+  question : "What is the Capital of Mississippi? ",
+  options : ["Oxford", "Mobile",  "Jackson"],
+  answer : "Jackson"
 }
+
+quizQuestion3 = {
+  question : "What is the Capital of Montana? ",
+  options : ["Butte", "Helena",  "Montana City", "Toronto"],
+  answer : "Helena"
+}
+
+quizArray = [quizQuestion0, quizQuestion1, quizQuestion2, quizQuestion3];
+quizQuestionNumber = 0;
+points = 0;
+
+createQuestion(quizQuestionNumber);
+
+
+submitButton.onclick = function(){
+  questionArray = document.getElementsByTagName('input');
+  for(y = 0; y < questionArray.length; y++){
+     if(questionArray[y].checked){
+      response = questionArray[y].value;
+    }
+  }
+  if( response == quizArray[quizQuestionNumber].answer){
+    points++;
+    alert('You rock, you have ' + points + ' points');
+  }else{
+    alert('Sorry :(, wrong answer');
+  }
+  quizQuestionNumber++;
+  if(quizQuestionNumber < quizArray.length){
+    createQuestion(quizQuestionNumber);
+  }else{
+    document.getElementById('quiz-area').className = 'hide';
+    document.getElementById('final').innerHTML = '<p>You are all done. <br> Your final score is ' + points + ' points<p>';
+  }
+}
+
+//functions//
+
+function createQuestion(quizQuestionNumber){
+  document.getElementById('question').innerHTML = quizArray[quizQuestionNumber].question;
+  submitButton = document.getElementById('submit-button');
+  questionField = document.getElementById('question-options');
+  questionNumbercounter = document.getElementById('question-number');
+  questionNumbercounter.innerHTML = "Question "+ quizQuestionNumber + " of "+ quizArray.length; 
+  questionField.innerHTML ="";
+
+  for(x = 0; x < quizArray[quizQuestionNumber].options.length; x++){
+    var input  = document.createElement('input');
+    var label  = document.createElement('label');
+    var lineBreak = document.createElement('br');
+    input.type = "radio";
+    input.name = "question"+quizQuestionNumber;
+    input.value = quizArray[quizQuestionNumber].options[x];
+    input.id = quizArray[quizQuestionNumber].options[x];
+    questionField.appendChild(input);
+    label.innerHTML = quizArray[quizQuestionNumber].options[x];
+    label.htmlFor = quizArray[quizQuestionNumber].options[x];
+    questionField.appendChild(label);
+    questionField.appendChild(lineBreak);
+  }
+}    
+
 
 
 
